201fa93ada82d34cc748a3e5edc01bbccd66f130
Manny Diaz <mannyd47@gmail.com>
2024-05-19 22:01:44 +0000

    final test

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,100 @@
+const galToL = 3.78541;
+const lbsToKg = 0.453592;
+const miToKm = 1.60934;
+
+units = {
+  gal: "L",
+  L: "gal",
+  mi: "km",
+  km: "mi",
+  lbs: "kg",
+  kg: "lbs",
+};
+
+conversionRate = {
+  gal: galToL,
+  L: 1 / galToL,
+  mi: miToKm,
+  km: 1 / miToKm,
+  lbs: lbsToKg,
+  kg: 1 / lbsToKg,
+};
+
+unitMapping = {
+  gal: "gallons",
+  L: "liters",
+  mi: "miles",
+  km: "kilometers",
+  lbs: "pounds",
+  kg: "kilograms",
+};
+
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+  this.getNum = function (word) {
+    const englishAlphabet = /[a-zA-Z]/;
+    const idx = word.split("").findIndex((char) => englishAlphabet.test(char));
+    if (idx === 0) {
+      return 1;
+    }
+
+    let quantityStr;
+    if (idx < 0) {
+      quantityStr = word.slice(0);
+    } else {
+      quantityStr = word.slice(0, idx);
+    }
+
+    const quantityArr = quantityStr.split("/");
+
+    if (quantityArr.length === 1) {
+      const quantity = quantityArr[0];
+      if (quantity === "") return "invalid number";
+      return isNaN(+quantity) ? "invalid number" : +quantity;
+    }
+    if (quantityArr.length === 2) {
+      if (quantityArr.some((num) => num === "")) {
+        return "invalid number";
+      }
+      const numerator = +quantityArr[0];
+      const denominator = +quantityArr[1];
+      return isNaN(numerator) || isNaN(denominator)
+        ? "invalid number"
+        : numerator / denominator;
+    }
+
+    return "invalid number";
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (word) {
+    const englishAlphabet = /[a-zA-Z]/;
+    const idx = word.split("").findIndex((char) => englishAlphabet.test(char));
+    if (idx < 0) {
+      return "invalid unit";
+    }
+    const unit = word.slice(idx);
+    return this.spellOutUnit(unit);
   };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+  this.getReturnUnit = function (initUnit) {
+    return units[initUnit];
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.spellOutUnit = function (unit) {
+    if (unit === "L" || unit === "l") return "L";
+    if (units.hasOwnProperty(unit.toLowerCase())) {
+      return unit.toLowerCase();
+    }
+    return "invalid unit";
   };
-  
+
+  this.convert = function (initNum, initUnit) {
+    return Math.round(conversionRate[initUnit] * initNum * 1e5) / 1e5;
+  };
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${unitMapping[initUnit]} converts to ${returnNum} ${unitMapping[returnUnit]}`;
+  };
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,39 @@
-'use strict';
+"use strict";
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
 
+  app.route("/api/convert").get((req, res) => {
+    const { input } = req.query;
+    const word = input;
+    if (!word) {
+      return res.send("invalid number and unit");
+    }
+    const initNum = convertHandler.getNum(word);
+    const initUnit = convertHandler.getUnit(word);
+
+    if (initNum === "invalid number" && initUnit === "invalid unit") {
+      return res.send("invalid number and unit");
+    }
+    if (initNum === "invalid number") {
+      return res.send("invalid number");
+    }
+    if (initUnit === "invalid unit") {
+      return res.send("invalid unit");
+    }
+
+    const returnUnit = convertHandler.getReturnUnit(initUnit);
+    const returnNum = convertHandler.convert(initNum, initUnit);
+    const string = convertHandler.getString(
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit
+    );
+    res.json({ initNum, initUnit, returnNum, returnUnit, string });
+  });
 };
